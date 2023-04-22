7005b245b39a50b9eca8b4156582255c7db83580
zanbin168 <zanbin168@qq.com>
2023-04-22 12:04:36 +0800

    update

--- time/default.go
+++ time/default.go
@@ -4,5 +4,6 @@ const (
 	FormatFullDateTime       = "2006-01-02 15:04:05"
 	FormatShortDateTime      = "06-01-02 15:04:05"
 	FormatShortMonthDateTime = "01-02 15:04:05"
+	FormatFullDate           = "2006-01-02"
 	Format_1                 = "0102-150505"
 )
