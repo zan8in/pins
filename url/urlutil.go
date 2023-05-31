3b6b80662275b2103473d0fcdda212936fbce359
zanbin168 <zanbin168@qq.com>
2023-05-31 15:57:51 +0800

    urlencode all char

--- url/urlutil.go
+++ url/urlutil.go
@@ -1,6 +1,7 @@
 package urlutil
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 )
@@ -33,4 +34,16 @@ func Host(s string) (string, error) {
 		return "", err
 	}
 	return u.Host, nil
+}
+
+// URL Encode all characters
+// URLEncodeAllChar("1' and sleep(5) and '1'='1")
+// %31%27%20%61%6E%64%20%73%6C%65%65%70%28%35%29%20%61%6E%64%20%27%31%27%3D%27%31
+func URLEncodeAllChar(s string) string {
+	b := []byte(s)
+	nb := ""
+	for _, v := range b {
+		nb += "%" + fmt.Sprintf("%X", v)
+	}
+	return nb
 }
