c13636d14719bc7b2903f8cd5d29b340ff18183f
zanbin168 <zanbin168@qq.com>
2023-06-11 14:45:57 +0800

    add csv

--- csv/csvutil.go
+++ csv/csvutil.go
@@ -7,9 +7,11 @@ import (
 	"os"
 
 	fileutil "github.com/zan8in/pins/file"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
-func ReadFile(filename string) (chan []string, error) {
+func ReadFile(filename, charset string) (chan []string, error) {
 	var err error
 
 	if !fileutil.FileExists(filename) {
@@ -26,7 +28,12 @@ func ReadFile(filename string) (chan []string, error) {
 		}
 		defer file.Close()
 
-		reader := csv.NewReader(file)
+		var reader *csv.Reader
+		if charset != "" && charset == "gbk" {
+			reader = csv.NewReader(transform.NewReader(file, simplifiedchinese.GBK.NewDecoder()))
+		} else {
+			reader = csv.NewReader(file)
+		}
 
 		for {
 			record, err := reader.Read()
--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/go-errors/errors v1.4.2
 	github.com/stretchr/testify v1.8.2
+	go.uber.org/multierr v1.11.0
+	golang.org/x/text v0.9.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
 )
