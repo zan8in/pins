920437d7fa86b75153ee8e7cb4d7743093f6e7b8
zanbin168 <zanbin168@qq.com>
2023-10-09 16:24:42 +0800

    update

--- time/timeutil.go
+++ time/timeutil.go
@@ -5,3 +5,10 @@ import "time"
 func Format(format string) string {
 	return time.Now().Format(format)
 }
+
+// 使用time.Parse函数来尝试将输入日期字符串解析为指定的日期格式。
+// 如果日期格式不正确，会返回一个错误，然后我们可以根据错误来判断日期格式是否正确。
+func ValidateDateFormat(value, layout string) bool {
+	_, err := time.Parse(layout, value)
+	return err == nil
+}
